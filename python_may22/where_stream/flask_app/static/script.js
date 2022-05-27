87e218554a3ffc2c168ef806de27c715d89d2f78
Sean McNeil <smcneil136@gmail.com>
2022-05-27 09:16:12 -0400

    Accessibility fixes, Added search bar to more pages, Materialize JS additions

--- python_may22/where_stream/flask_app/static/script.js
+++ python_may22/where_stream/flask_app/static/script.js
@@ -1,3 +1,11 @@
+$(document).ready(function() {
+    $('input#input_text, textarea#textarea2').characterCounter();
+});
+
+$(document).ready(function() {
+    M.updateTextFields();
+});
+
 $(document).ready(function(){
     $('.collapsible').collapsible();
 });
